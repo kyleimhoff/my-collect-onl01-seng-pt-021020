401908dfd022abda6146908713ea40083d1cdd2d
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-02-19 21:24:39 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,19 @@
+def my_collect(array)
+  if block_given?
+     i = 0 
+     my_collection = []
+     while i < array.length 
+     my_collection << yield(array[i])
+     i = i + 1 
+     end 
+    my_collection
+  
+  else
+  puts "there is no block."
+  end
+end  
 
+my_collect(caller_locations) do |name|
+  name
+end
 
